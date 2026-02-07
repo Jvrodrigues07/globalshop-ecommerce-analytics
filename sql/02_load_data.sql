d9684dc40cfb6b636fb6b3551d247f0149578383
Jvrodrigues07 <jvrodriguesb@hotmail.com>
2026-02-07 17:44:23 -0300

    Add bulk data loading script

--- sql/02_load_data.sql
+++ sql/02_load_data.sql
@@ -1,16 +1,21 @@
+-- Phase 1: Bulk data loading (edit file paths as needed)
 SET GLOBAL local_infile = 1;
 
+-- orders
 LOAD DATA LOCAL INFILE 'olist_orders_dataset.csv'
 INTO TABLE orders
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 IGNORE 1 LINES;
 
+-- order_items
 LOAD DATA LOCAL INFILE 'olist_order_items_dataset.csv'
 INTO TABLE order_items
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 IGNORE 1 LINES;
 
+-- payments
 LOAD DATA LOCAL INFILE 'olist_order_payments_dataset.csv'
 INTO TABLE payments
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 IGNORE 1 LINES;
+
